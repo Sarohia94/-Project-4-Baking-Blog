d371bc473d621076e750d5f84f539f70cf80fda4
Sarohia94 <sarohia94@gmail.com>
2023-05-07 18:28:52 +0000

    Update post admin class and add classes for comment and recipe admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,39 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Recipe
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):  
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'title', 'created_on')
     summernote_fields = ('content')
+    actions = ['approve_posts']
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Recipe)  # port 8000 not found error
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'title', 'created_on')
+    actions = ['approve_recipe']
+
+    def approve_recipe(self, request, queryset):
+        queryset.update(approved=True)
